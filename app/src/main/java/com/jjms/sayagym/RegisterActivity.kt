a0d13a4b0b1264804b0047b4e9d50be6e2f5fce5
JavierMarSan|8 <marroquinjavier75@gmail.com>
2021-11-05 11:33:07 -0600

    Login complete

--- app/src/main/java/com/jjms/sayagym/RegisterActivity.kt
+++ app/src/main/java/com/jjms/sayagym/RegisterActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.View
 import android.widget.EditText
-import android.widget.ProgressBar
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -15,31 +13,70 @@ import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
 
-   /* private lateinit var txtName : EditText
+    private lateinit var txtName : EditText
     private lateinit var txtEmail : EditText
     private lateinit var txtPassword : EditText
-    private lateinit var progressBar : ProgressBar
-    private lateinit var dbReference: DatabaseReference
+    private lateinit var auth : FirebaseAuth
     private lateinit var database : FirebaseDatabase
-    private lateinit var auth : FirebaseAuth*/
+    private lateinit var dbReference : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        /*txtName = findViewById(R.id.txtName)
+        txtName = findViewById(R.id.txtName)
         txtEmail = findViewById(R.id.txtEmail)
         txtPassword = findViewById(R.id.txtPassword)
-        txtEmail = findViewById(R.id.txtEmail)
-
-        progressBar = findViewById(R.id.progressBar)
 
         database = FirebaseDatabase.getInstance()
-        auth = FirebaseAuth.getInstance()
+        dbReference = database.reference.child("User")
 
-        dbReference = database.reference.child("User")*/
+        auth = FirebaseAuth.getInstance()
     }
 
-    fun onClickRegister(view: android.view.View) {}
+    fun onClickRegister(view: android.view.View) {
+        createNewAccount()
+    }
 
+    private fun createNewAccount(){
+        val name = txtName.text.toString()
+        val email = txtEmail.text.toString()
+        val password = txtPassword.text.toString()
+
+        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){
+                    task ->
+                    if(task.isSuccessful){
+                        val user : FirebaseUser? = auth.currentUser
+                        verifyEmail(user)
+
+                        val userDB = user?.uid?.let { dbReference.child(it) }
+
+                        userDB?.child("Name")?.setValue(name)
+
+                        action()
+                    }
+                }
+        }else{
+            Toast.makeText(this, "Campos vacios", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun verifyEmail(user : FirebaseUser?){
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this){
+                task ->
+                if(task.isSuccessful){
+                    Toast.makeText(this, "Correo Enviado", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "Error al enviar correo", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun action(){
+        val createActivity = Intent(this, LoginActivity::class.java)
+        startActivity(createActivity)
+    }
 }
